8ba2b42f3974a43bc037db1c8afc8f60a9c499fd
tzabalo <t.zabalo@cdg82.fr>
2025-05-05 14:05:47 +0200

    Basic angular components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-header/>
+    <main>
+      <router-outlet/>
+    </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
